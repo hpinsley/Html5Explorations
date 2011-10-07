251863eff1aac1a8c3a2494faa38374ce207aa81
Howard Pinsley <hpinsley@gmail.com>
2011-10-06 20:21:10 -0400

    Had to implement manual scaling to avoid the canvas scaling up line widths.

--- JSWebPlay/Scripts/Graph/Graph2d.js
+++ JSWebPlay/Scripts/Graph/Graph2d.js
@@ -8,9 +8,14 @@
     var origin = new Point(0, 0);
 
     plotter.createCanvas(origin, bounds);
-    plotter.drawLine(new Point(0, 0), new Point(100, 100));
-    plotter.drawLine(new Point(0, 50), new Point(100, 150));
-
+    //plotter.drawLine(new Point(-3, 0), new Point(3, 0));
+    var x0 = -3.14;
+    var x1 = 3.14;
+    var y0 = -3.14;
+    var y1 = 3.14;
+    plotter.setWorldCoordinates(x0, x1, y0, y1);
+    plotter.drawLine(new Point(0, -3), new Point(0, 3));
+    plotter.drawLine(new Point(-3, 0), new Point(3, 0));
 });
 
 
--- JSWebPlay/Scripts/Graph/Plotter.js
+++ JSWebPlay/Scripts/Graph/Plotter.js
@@ -6,7 +6,7 @@
 function Bounds(width, height) {
     this.width = width;
     this.height = height;
-}
+};
 
 function Plotter() {
 };
@@ -16,7 +16,7 @@ Plotter.prototype.createCanvas = function (origin, bounds) {
     this.bounds = bounds;
     this.canvas = $("<canvas/>")
                     .css({ display: "block",
-                        "background-color": "blue",
+                        "background-color": "white",
                         position: "relative",
                         left: origin.x, top: origin.y,
                         width: bounds.width, height: bounds.height
@@ -27,14 +27,38 @@ Plotter.prototype.createCanvas = function (origin, bounds) {
     el.clientWidth = el.width = bounds.width;
     el.clientHeight = el.height = bounds.height;
     this.ctx = this.canvas[0].getContext("2d");
+    //Move the device coordinate orgin in the center
+    this.ctx.translate(bounds.width / 2, bounds.height / 2);
+};
 
+//Given world coordinates, set a transformation function.
+//We can't use the inherent canvas transforms because they
+//affect the line width.
+//
+//Once set, use the Plotter instances worldToDevice function
+
+Plotter.prototype.setWorldCoordinates = function (x0, x1, y0, y1) {
+    this.worldToDevice = function (worldPoint) {
+        var dx0 = -this.bounds.width / 2;
+        var dx1 = this.bounds.width / 2;
+        var dy0 = -this.bounds.height / 2;
+        var dy1 = this.bounds.height / 2;
+
+        return new Point(
+            dx0 + (dx1 - dx0) * (worldPoint.x - x0) / (x1 - x0),
+            dy0 + (dy1 - dy0) * (worldPoint.y - y0) / (y1 - y0)
+        );
+    };
 };
 
 
 Plotter.prototype.drawLine = function (from, to) {
+    var dFrom = this.worldToDevice(from);
+    var dTo = this.worldToDevice(to);
+    
     var ctx = this.ctx;
     ctx.beginPath();
-    ctx.moveTo(from.x, from.y);
-    ctx.lineTo(to.x, to.y);
+    ctx.moveTo(dFrom.x, dFrom.y);
+    ctx.lineTo(dTo.x, dTo.y);
     ctx.stroke();
 }
